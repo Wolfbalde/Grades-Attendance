d65e8edcbc5452d6865d8f843dd1286870932b16
Wolfbalde <akaashbadi18@gmail.com>
2024-05-12 23:17:56 +0530

    Quiz inclusion
    The Quizzes attended by the student are also considered towards the final grade calculation

--- Grades.py
+++ Grades.py
@@ -13,6 +13,12 @@ marks_list = pd.read_csv(
 )
 marks_list = marks_list.sort_values(by="Roll No")
 
+quiz_list = pd.read_csv(
+    'C:/Users/Akaash/Desktop/Grades/Grades-Attendance/Quiz.csv',
+    usecols=["Email", "Name", "Roll No", "Quiz 1", "Quiz 2", "Quiz 3"],
+)
+quiz_list = quiz_list.sort_values(by="Roll No")
+
 attd_list = pd.read_csv(
     'C:/Users/Akaash/Desktop/Grades/Grades-Attendance/Attendance.csv',
     usecols=["Email", "Name", "Roll No", "Attendance"],
@@ -21,15 +27,17 @@ attd_list = attd_list.sort_values(by="Roll No")
 
 # Merging DataFrames
 final_data = pd.merge(stud_list, marks_list, on=["Email", "Name", "Roll No"], how="outer")
+final_data = pd.merge(final_data,quiz_list, on=["Email", "Name", "Roll No"], how="outer")
 final_data = pd.merge(final_data, attd_list, on=["Email", "Name", "Roll No"], how="outer")
 final_data = final_data.fillna(0)
 
 #mapping grades based on the marks scored
 grades = {
-    90: "A",
-    80: "B",
-    70: "C",
-    60: "D",
+    90: "A+",
+    80: "A",
+    70: "B",
+    60: "C",
+    50: "D",
     0: "F",
 }
 
@@ -40,12 +48,11 @@ def grade_mapping(value):
 
 
 #display the mark percentage and attendance percentage
-def student_attendance_grade_percentage(mark,attendance):
-    mark_percentage = mark*100
+def student_attendance_grade_percentage(mark,attendance,quiz):
     attendance_percentage = (attendance/40)*100
-    grade = grade_mapping(mark_percentage)
-    print("The percentage of this student is: ", round(mark_percentage,2)," and the attendance percentage is: ",  round(attendance_percentage,2)
-          ,"and the grade of this student is: ", grade)
+    grade = (mark*0.5)+(quiz*0.5)
+    print("The attendance percentage is: ",  round(attendance_percentage,2)
+          ,"and the grade of this student is: ", grade_mapping(grade*100))
 
 
 #Code starts here
@@ -62,8 +69,12 @@ while bol:
         mark2 = row["Marks2"].iloc[0]
         mark3 = row["Marks3"].iloc[0]
         mark = (mark1+mark2+mark3)/270
+        quiz1 = row["Quiz 1"].iloc[0]
+        quiz2 = row["Quiz 2"].iloc[0]
+        quiz3 = row["Quiz 3"].iloc[0]
+        quiz=(quiz1+quiz2+quiz3)/60
         attend=row["Attendance"].iloc[0]
-        student_attendance_grade_percentage(mark,attend)
+        student_attendance_grade_percentage(mark,attend,quiz)
         cont=input("Do you wish to continue? (y/n): ")
         if cont=="y" or cont=="Y":
             bol=True
